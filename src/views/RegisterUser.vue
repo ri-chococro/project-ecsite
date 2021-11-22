a265f599ce20252c9614af2d6240140e5035c79c
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-22 12:09:36 +0900

    templateにエラーメッセージを追加

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -2,8 +2,9 @@
   <div class="register-user">
     <div class="container">
       <div class="row register-page">
-        <div class="error">エラーメッセージ</div>
+        <div class="error">{{ registerOfError }}</div>
         <div class="row">
+          <div class="error">{{ nameOfError }}</div>
           <div class="input-field col s6">
             <input id="last_name" type="text" class="validate" required />
             <label for="last_name">姓</label>
@@ -14,12 +15,14 @@
           </div>
         </div>
         <div class="row">
+          <div class="error">{{ emailOfError }}</div>
           <div class="input-field col s12">
             <input id="email" type="email" class="validate" required />
             <label for="email">メールアドレス</label>
           </div>
         </div>
         <div class="row">
+          <div class="error">{{ zipCodeOfError }}</div>
           <div class="input-field col s12">
             <input id="zipcode" type="text" maxlength="7" />
             <label for="zipcode">郵便番号(ハイフンなし)</label>
@@ -29,18 +32,21 @@
           </div>
         </div>
         <div class="row">
+          <div class="error">{{ addressOfError }}</div>
           <div class="input-field col s12">
             <input id="address" type="text" />
             <label for="address">住所</label>
           </div>
         </div>
         <div class="row">
+          <div class="error">{{ telephoneOfError }}</div>
           <div class="input-field col s12">
             <input id="tel" type="tel" />
             <label for="tel">電話番号</label>
           </div>
         </div>
         <div class="row">
+          <div class="error">{{ passwordOfError }}</div>
           <div class="input-field col s12">
             <input
               id="password"
@@ -53,6 +59,7 @@
           </div>
         </div>
         <div class="row">
+          <div class="error">{{ confirmationOfError }}</div>
           <div class="input-field col s12">
             <input
               id="confirmation_password"
@@ -98,6 +105,7 @@ export default class RegisterUser extends Vue {
   private telephoneOfError = "";
   private passwordOfError = "";
   private confirmationOfError = "";
+  private registerOfError = "";
 }
 </script>
 
