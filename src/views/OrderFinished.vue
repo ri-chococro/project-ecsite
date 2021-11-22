e0e6d90d5bde239db15d90d67908c03b6cdef648
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-22 13:59:59 +0900

    注文完了画面を作成

---
+++ src/views/OrderFinished.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="container">
+      <h1 class="page-title">決済が完了しました！</h1>
+      <div class="order-finished-thanks-message">
+        <p>この度はご注文ありがとうございます。</p>
+        <p>
+          お支払い先は、お送りしたメールに記載してありますのでご確認ください。
+        </p>
+        <p>メールが届かない場合は「注文履歴」からご確認ください。</p>
+      </div>
+      <div class="row order-finished-btn">
+        <button class="btn" type="button" v-on:click="onClickToTop">
+          <span>トップ画面を表示する</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class OrderFinished extends Vue {
+  onClickToTop(): void {
+    this.$router.push("/itemList");
+  }
+}
+</script>
+
+<style scoped>
+.order-finished-thanks-message {
+  text-align: center;
+}
+
+.order-finished-btn {
+  text-align: center;
+}
+</style>
