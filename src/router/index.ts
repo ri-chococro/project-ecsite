d3467ab8bfddddc0800bb4ec9f765de843a855ff
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2021-11-22 13:18:33 +0900

    ヘッダー、フッターを修正しました

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,14 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+  //パスが登録されていない場合はログイン画面へ
+  // {
+  //   path: "*",
+  //   component: () => import("../views/Login.vue"),
+  // },
 ];
 
 const router = new VueRouter({
