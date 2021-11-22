eabd6f6ccfb24eda70970604068d3618019b30e6
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-22 14:24:02 +0900

    ショッピングカートリストファイルを作成

---
+++ src/views/CartList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="cart-list"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class CartList extends Vue {}
+</script>
+
+<style scoped></style>
