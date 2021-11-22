aab74a3e69cdd7aa44770e44d7ef8eb8937c9602
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-22 17:55:36 +0900

    stateにカートに入っているitemを入れる配列を用意、gettersにカートに入っているアイテムをとってくるメソッドを追加

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,20 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { OrderItem } from "@/types/orderItem";
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: true,
   state: {
+    items: new Array<OrderItem>(),
   },
-  mutations: {
+  mutations: {},
+  getters: {
+    getItemsInCart(state) {
+      return state.items;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
