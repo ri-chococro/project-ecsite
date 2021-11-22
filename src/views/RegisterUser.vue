407d8230c33b6adc3e3645c6a114abcca27d6336
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-22 13:58:26 +0900

    templateにv-model、v-on:clickを追加

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -6,25 +6,43 @@
         <div class="row">
           <div class="error">{{ nameOfError }}</div>
           <div class="input-field col s6">
-            <input id="last_name" type="text" class="validate" required />
+            <input
+              id="last_name"
+              type="text"
+              class="validate"
+              v-model="lastName"
+              required
+            />
             <label for="last_name">姓</label>
           </div>
           <div class="input-field col s6">
-            <input id="first_name" type="text" class="validate" required />
+            <input
+              id="first_name"
+              type="text"
+              class="validate"
+              v-model="firstName"
+              required
+            />
             <label for="first_name">名</label>
           </div>
         </div>
         <div class="row">
           <div class="error">{{ emailOfError }}</div>
           <div class="input-field col s12">
-            <input id="email" type="email" class="validate" required />
+            <input
+              id="email"
+              type="email"
+              class="validate"
+              v-model="emailAddress"
+              required
+            />
             <label for="email">メールアドレス</label>
           </div>
         </div>
         <div class="row">
           <div class="error">{{ zipCodeOfError }}</div>
           <div class="input-field col s12">
-            <input id="zipcode" type="text" maxlength="7" />
+            <input id="zipcode" type="text" maxlength="7" v-model="zipCode" />
             <label for="zipcode">郵便番号(ハイフンなし)</label>
             <button class="btn" type="button">
               <span>住所検索</span>
@@ -34,14 +52,14 @@
         <div class="row">
           <div class="error">{{ addressOfError }}</div>
           <div class="input-field col s12">
-            <input id="address" type="text" />
+            <input id="address" type="text" v-model="address" />
             <label for="address">住所</label>
           </div>
         </div>
         <div class="row">
           <div class="error">{{ telephoneOfError }}</div>
           <div class="input-field col s12">
-            <input id="tel" type="tel" />
+            <input id="tel" type="tel" v-model="telephone" />
             <label for="tel">電話番号</label>
           </div>
         </div>
@@ -53,6 +71,7 @@
               type="password"
               class="validate"
               minlength="8"
+              v-model="password"
               required
             />
             <label for="password">パスワード</label>
@@ -66,17 +85,14 @@
               type="password"
               class="validate"
               minlength="8"
+              v-model="confirmationPassword"
               required
             />
             <label for="confirmation_password">確認用パスワード</label>
           </div>
         </div>
         <div class="row register-admin-btn">
-          <button
-            class="btn"
-            type="button"
-            onclick="location.href='login.html'"
-          >
+          <button class="btn" type="button" v-on:click="registerUser">
             <span>登録<i class="material-icons right">done</i></span>
           </button>
         </div>
@@ -157,6 +173,10 @@ export default class RegisterUser extends Vue {
     }
     if (this.address === "") {
       this.addressOfError = "住所が入力されていません";
+      hasError = true;
+    }
+    if (this.telephone === "") {
+      this.telephoneOfError = "電話番号が入力されていません";
       hasError = true;
     }
     if (this.password === "") {
