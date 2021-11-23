dcd859fce73e34f0aee4290d17cf1a65d99ed234
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-23 20:30:49 +0900

    template内の表示をv-forに変更 createdでストアからカート内アイテムを取得、合計金額を取得する記述を追加

--- src/views/CartList.vue
+++ src/views/CartList.vue
@@ -15,7 +15,46 @@
               </tr>
             </thead>
             <tbody>
-              <tr>
+              <tr
+                v-for="currentCartItem of currentCartItems"
+                v-bind:key="currentCartItem.id"
+              >
+                <td class="cart-item-name">
+                  <div class="cart-item-icon">
+                    <img v-bind:src="currentCartItem.item.imagePath" />
+                  </div>
+                  <span>{{ currentCartItem.item.name }}</span>
+                </td>
+                <td>
+                  <span class="price">&nbsp;{{ currentCartItem.size }}</span
+                  >&nbsp;&nbsp;{{ currentCartItem.itemPrice }}円 &nbsp;&nbsp;{{
+                    currentCartItem.quantity
+                  }}個
+                </td>
+                <td>
+                  <ul
+                    v-for="topping of currentCartItem.orderToppingList"
+                    v-bind:key="topping.id"
+                  >
+                    <li>
+                      {{ topping.Topping.name
+                      }}{{ currentCartItem.toppingPrice }}円
+                    </li>
+                  </ul>
+                </td>
+                <td>
+                  <div class="text-center">
+                    {{ currentCartItem.calcSubTotalPrice.toLocaleString() }}円
+                  </div>
+                </td>
+                <td>
+                  <button class="btn" type="button">
+                    <span>削除</span>
+                  </button>
+                </td>
+              </tr>
+
+              <!-- <tr>
                 <td class="cart-item-name">
                   <div class="cart-item-icon">
                     <img src="img/1.jpg" />
@@ -64,32 +103,7 @@
                     <span>削除</span>
                   </button>
                 </td>
-              </tr>
-              <tr>
-                <td class="cart-item-name">
-                  <div class="cart-item-icon">
-                    <img src="img/1.jpg" />
-                  </div>
-                  <span>ハワイアンパラダイス</span>
-                </td>
-                <td>
-                  <span class="price">&nbsp;Ｌ</span>&nbsp;&nbsp;2,380円
-                  &nbsp;&nbsp;1個
-                </td>
-                <td>
-                  <ul>
-                    <li>ピーマン300円</li>
-                    <li>オニオン300円</li>
-                    <li>あらびきソーセージ300円</li>
-                  </ul>
-                </td>
-                <td><div class="text-center">3,280円</div></td>
-                <td>
-                  <button class="btn" type="button">
-                    <span>削除</span>
-                  </button>
-                </td>
-              </tr>
+              </tr> -->
             </tbody>
           </table>
         </div>
@@ -115,10 +129,28 @@
 </template>
 
 <script lang="ts">
+import { OrderItem } from "@/types/orderItem";
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class CartList extends Vue {}
+export default class CartList extends Vue {
+  private currentCartItems: Array<OrderItem> = [];
+  private totalPrice = 0;
+  private taxPrice = 0;
+  private noItem = true;
+
+  created(): void {
+    this.currentCartItems = this.$store.getters.getItemsInCart();
+    if (this.currentCartItems.length === 0) {
+      this.noItem = false;
+    }
+    for (let currentCartItem of this.currentCartItems) {
+      this.totalPrice += currentCartItem.calcSubTotalPrice;
+    }
+    const tax = 0.1;
+    this.taxPrice = Math.floor(this.totalPrice * tax);
+  }
+}
 </script>
 
 <style scoped>
