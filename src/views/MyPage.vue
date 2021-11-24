4d20a240c0c5db0c10090f0aca013cd20cddf01c
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-24 14:05:52 +0900

    ユーザー情報表示部分を実装（レイアウト）

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="container">
-    <img src="" />
-    <div class="icon-name">
-      <img src="img/no-profile.png" class="profile-img" />
-      <div class="name">山田太郎</div>
-    </div>
-    <div>
-      <div class="title">登録情報</div>
-      <div class="user-info">
-        <p>メールアドレス: example.com</p>
-        <p>住所: 東京都新宿区〇〇</p>
-        <p>電話番号: 000-0000-0000</p>
+    <div class="profile-wrapper">
+      <div class="icon-name">
+        <img src="img/no-profile.png" class="profile-img" />
+        <div class="name">{{ loginUser.name }}</div>
       </div>
-    </div>
-    <div class="order-history">
-      <div class="title">注文履歴</div>
+      <div>
+        <div class="title">登録情報</div>
+        <div class="user-info">
+          <p><i class="fas fa-envelope fa-lg icon"></i>メールアドレス</p>
+          <p class="info">{{ loginUser.email }}</p>
+          <p><i class="fas fa-map-marker-alt fa-lg icon"></i>住所</p>
+          <p class="info">{{ loginUser.address }}</p>
+          <p><i class="fas fa-phone fa-lg icon"></i>電話番号</p>
+          <p class="info">{{ loginUser.telephone }}</p>
+        </div>
+      </div>
+      <div class="order-history">
+        <div class="title">注文履歴</div>
+      </div>
     </div>
   </div>
 </template>
@@ -25,7 +29,12 @@ export default {};
 
 <style scoped>
 .container {
+  background-color: #f7f7f7;
+}
+.profile-wrapper {
   width: 40%;
+  margin: 0 auto;
+  padding: 30px 0;
 }
 .icon-name {
   display: flex;
@@ -48,10 +57,18 @@ export default {};
 .user-info {
   /* border: 1px solid #ccc; */
   padding: 20px;
-  /* box-shadow: black 2px 2px 5px; */
+  box-shadow: black 2px 2px 5px;
   font-size: 18px;
+  margin-top: 20px;
 }
 
-.order-history {
+.icon {
+  color: rgb(65, 146, 65);
+  margin-right: 30px;
+}
+
+.info {
+  margin-left: 50px;
+  border-bottom: 1px solid #ccc;
 }
 </style>
