910ee92dfd783cebbae6ed523ff615c6e20c3b63
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-25 11:54:10 +0900

    ログイン時にユーザー情報を取得する処理を修正

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -101,22 +101,21 @@ export default class Login extends Vue {
     if (response.data.status === "success") {
       // ログイン状態にする(stateのisLoginをtrueにする)
       this.$store.commit("logined");
+      
+      let user = response.data.responseMap.user;
       this["$store"].commit("setLoginUser", {
-        user: {
-          id: response.data.id,
-          name: response.data.name,
-          email: response.data.email,
-          password: response.data.password,
-          zipcode: response.data.zipcode,
-          address: response.data.address,
-          telephone: response.data.telephone,
-        },
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        zipcode: user.zipcode,
+        address: user.address,
+        telephone: user.telephone,
       });
       this["$router"].push("/itemList");
     } else if (response.data.status === "error") {
       this.mismatchError = response.data.message;
     }
-    console.log(response);
   }
 }
 </script>
