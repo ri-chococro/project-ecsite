6c31c4c4f99f69dbc752fbda36ec31e65751d814
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-26 10:28:14 +0900

    商品・トッピング金額を個数に合わせた表示に修正

--- src/views/OrderConfirm.vue
+++ src/views/OrderConfirm.vue
@@ -27,9 +27,9 @@
                 </td>
                 <td>
                   <span class="price">&nbsp;{{ currentCartItem.size }}</span
-                  >&nbsp;&nbsp;{{ currentCartItem.itemPrice }}円 &nbsp;&nbsp;{{
-                    currentCartItem.quantity
-                  }}個
+                  >&nbsp;&nbsp;{{
+                    currentCartItem.itemPrice.toLocaleString()
+                  }}円 &nbsp;&nbsp;{{ currentCartItem.quantity }}個
                 </td>
                 <td>
                   <ul
@@ -38,7 +38,7 @@
                   >
                     <li>
                       {{ topping.topping.name
-                      }}{{ currentCartItem.toppingPrice }}円
+                      }}{{ currentCartItem.toppingPrice.toLocaleString() }}円
                     </li>
                   </ul>
                 </td>
