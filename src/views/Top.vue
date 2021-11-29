540c26c76d47d8f3899f424061cb64c80b796e85
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2021-11-29 10:06:30 +0900

    微修正

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -2,61 +2,86 @@
   <div class="top">
     <div class="top-wrapper">
       <div class="container">
-        <h1>DELIVERING THE FEELINGS OF ALOHA TO YOU</h1>
-        <p>日本にいながらアロハな気分をあなたにお届け！</p>
-        <p>
-          ラクラクアロハは自宅にハワイアン料理がデリバリーで届くハワイアンECサイトです。
-        </p>
-        <div class="btn-wrapper">
-          <a href="/registerUser" class="btn signup">会員登録</a>
-          <p>or</p>
-          <a href="#" class="btn facebook"
-            ><i class="fab fa-facebook"></i>Facebookで登録</a
-          >
-          <a href="#" class="btn twitter"
-            ><i class="fab fa-twitter"></i>Twitterで登録</a
-          >
-        </div>
+        <div class="title">Welcome to Rakus Hawaiians !!</div>
+        <div class="subTitle">Let's enjoy Hawaii feeling</div>
       </div>
     </div>
+    <div class="midle-wrapper">
+      <div class="appli">
+        <div class="googleMap">
+          <h2>Map Information</h2>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.4999497490426!2d139.7020887508128!3d35.68931288009525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d9c8bc1bfbb%3A0xcb44f68a614c714a!2z5qCq5byP5Lya56S-44Op44Kv44K544OR44O844OI44OK44O844K6!5e0!3m2!1sja!2sjp!4v1637912752420!5m2!1sja!2sjp"
+            width="500"
+            height="350"
+            style="border: 0"
+            allowfullscreen=""
+            loading="lazy"
+          ></iframe>
+        </div>
 
+        <div class="youtube">
+          <h2>Reference Video</h2>
+          <iframe
+            width="500"
+            height="350"
+            src="https://www.youtube.com/embed/TMyaoyRn_kk"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+      <a class="btn btn-custom01">
+        <span class="btn-custom01-front">
+          <a href="https://ja-jp.facebook.com/">Facebookで登録</a></span
+        ></a
+      >
+      <a class="btn btn-custom01">
+        <span class="btn-custom01-front">
+          <a href="https://twitter.com/?lang=ja">Twitterで登録</a></span
+        ></a
+      >
+    </div>
     <div class="top-item-wrapper">
       <div class="container">
         <div class="heading">
           <h2>ラクラクアロハ、４つの特徴！</h2>
         </div>
+
         <div class="top-items">
           <div class="top-item">
+            <p class="pickUp">ハワイアン料理</p>
             <div class="top-item-icon">
               <img src="img/1.jpg" />
-              <p>ハワイアン料理</p>
             </div>
             <p class="text-contents">
               アロハテーブルやウルフギャング、田中オブ東京などのハワイの有名店で修行を積んだ一流シェフによる本格的なハワイアン料理が家にいてもお手軽に楽しむことができます。
             </p>
           </div>
           <div class="top-item">
+            <p class="pickUp">スイーツも充実</p>
             <div class="top-item-icon">
               <img src="img/2.jpg" />
-              <p>スイーツも充実</p>
             </div>
             <p class="text-contents">
               アイランドビンテージコーヒーやボガーツカフェのアサイボウルはもちろんのこと、最近流行りのバナンのソフトクリーム、エッグスンシングスのふわふわパンケーキなど有名店のスイーツも充実しています。
             </p>
           </div>
           <div class="top-item">
+            <p class="pickUp">アルコールも豊富</p>
             <div class="top-item-icon">
               <img src="img/14.jpg" />
-              <p>アルコールも豊富</p>
             </div>
             <p class="text-contents">
               ハワイといえばビーチでマイタイ！コナブリューイングのハワイアンコナビールなどハワイでしか飲めないアルコールも豊富にあります。
             </p>
           </div>
           <div class="top-item">
+            <p class="pickUp">ホテル創作料理</p>
             <div class="top-item-icon">
               <img src="img/13.jpg" />
-              <p>ホテル創作料理</p>
             </div>
             <p class="text-contents">
               シェラトンワイキキ、ロイヤルハワイアン、モアナサーフライダー、アウラニに入っているレストランの創作料理も「ラクラクアロハ」なら気軽に注文できます。
@@ -70,7 +95,7 @@
       <div class="container">
         <div class="heading">
           <h2>さぁ、今すぐALOHAの旅に出よう！</h2>
-          <h3>Let's go on an Aloha trip!</h3>
+          <a href="/itemList" class="btn signup">Let's go on an Aloha trip!</a>
         </div>
         <span class="btn message">ALOHAの旅に出る！</span>
       </div>
@@ -86,9 +111,199 @@ export default class Top extends Vue {}
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Rampart+One&family=Yellowtail&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Anton&family=Rampart+One&family=Yellowtail&display=swap");
+@import "https://use.fontawesome.com/releases/v5.13.0/css/all.css";
+
+.googleMap {
+  padding: 50px;
+  margin-right: 750px;
+  width: 50px;
+}
+.youtube {
+  padding: 50px;
+  width: 50px;
+}
+.appli {
+  display: flex;
+  justify-content: left;
+}
+
+*,
+*:before,
+*:after {
+  -webkit-box-sizing: inherit;
+  box-sizing: inherit;
+}
+
+html {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  font-size: 62.5%;
+}
+
+*,
+*:before,
+*:after {
+  -webkit-box-sizing: inherit;
+  box-sizing: inherit;
+}
+
+html {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  font-size: 62.5%;
+}
+.pickUp {
+  font-size: 25px;
+  color: red;
+}
+
+.btn,
+a.btn,
+button.btn {
+  font-size: 1.6rem;
+  font-weight: 700;
+  line-height: 1.5;
+  position: relative;
+  display: inline-block;
+  padding: 1rem 4rem;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+  text-align: center;
+  vertical-align: middle;
+  text-decoration: none;
+  letter-spacing: 0.1em;
+  color: #212529;
+  border-radius: 0.5rem;
+}
+
+a.btn-custom01 {
+  margin-bottom: 0.5rem;
+  padding: 0;
+  border-radius: 0.75rem;
+}
+
+a.btn-custom01:before {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  content: "";
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+  -webkit-transform: translate3d(0, 0.75rem, -1rem);
+  transform: translate3d(0, 0.75rem, -1rem);
+
+  border: 2px solid #000;
+  border-radius: inherit;
+  background: #ccc100;
+  -webkit-box-shadow: 0 0.6rem 0 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0.6rem 0 0 rgba(0, 0, 0, 0.2);
+}
+
+a.btn-custom01:after {
+  font-family: "Font Awesome 5 Free";
+  font-size: 2rem;
+  font-weight: normal;
+  line-height: 1;
+  position: absolute;
+  top: calc(50% - 1rem);
+  left: 1.5rem;
+  margin: 0;
+  padding: 0;
+  content: "\f0e0";
+}
+
+.btn-custom01-front {
+  position: relative;
+  display: block;
+  padding: 1.5rem 5rem 1.5rem 5rem;
+
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+  border: 2px solid #000;
+  border-radius: inherit;
+  background: #fff100;
+}
+
+.fa-position-left {
+  position: absolute;
+  top: calc(50% - 0.5em);
+  left: 1rem;
+}
+
+.fa-position-right {
+  position: absolute;
+  top: calc(50% - 0.5em);
+  right: 1rem;
+}
+
+a.btn-custom01:hover {
+  -webkit-transform: translate(0, 0.25rem);
+  transform: translate(0, 0.25rem);
+  background: #fff100;
+}
+
+a.btn-custom01:hover:before {
+  -webkit-transform: translate3d(0, 0.5rem, -1rem);
+  transform: translate3d(0, 0.5rem, -1rem);
+  -webkit-box-shadow: 0 0.35rem 0 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0.35rem 0 0 rgba(0, 0, 0, 0.2);
+}
+
+a.btn-custom01:hover:after {
+  content: "\f2b6";
+}
+
+a.btn-custom01:active {
+  -webkit-transform: translate(0rem, 0.75rem);
+  transform: translate(0rem, 0.75rem);
+}
+
+a.btn-custom01:active:before {
+  -webkit-transform: translate3d(0, 0, -1rem);
+  transform: translate3d(0, 0, -1rem);
+  -webkit-box-shadow: 0 0.35rem 0 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0.35rem 0 0 rgba(0, 0, 0, 0.2);
+}
+
+a.btn-custom01:active:after {
+  content: "\f2b6";
+}
+
+.title {
+  font-family: "Rampart One", cursive;
+  font-family: "Yellowtail", cursive;
+  font-size: 100px;
+}
+.subTitle {
+  font-family: "Anton", sans-serif;
+  font-family: "Rampart One", cursive;
+  font-family: "Yellowtail", cursive;
+  font-size: 60px;
+}
 .top-wrapper {
   padding: 180px 0 500px 0; /* 上はヘッダが来るのでその分180px分空ける */
   background-image: url(/img/hawaii4.jpg/); /* 背景画像設定 */
+  background-size: cover; /* 画像の縦横比を崩すことなく、画像ができるだけ大きくなるよう拡大縮小 */
+  color: white;
+  text-align: center;
+  margin-bottom: -100px;
+}
+.midle-wrapper {
+  padding: 0px 0 100px 0;
+  background-image: url(/img/hawaii5.jpg/); /* 背景画像設定 */
   background-size: cover; /* 画像の縦横比を崩すことなく、画像ができるだけ大きくなるよう拡大縮小 */
   color: white;
   text-align: center;
@@ -115,15 +330,6 @@ export default class Top extends Vue {}
 
 .signup {
   background-color: #239b76;
-}
-
-.facebook {
-  background-color: #3b5998;
-  margin-right: 10px;
-}
-
-.twitter {
-  background-color: #55acee;
 }
 
 .btn {
@@ -159,14 +365,14 @@ export default class Top extends Vue {}
 .top-items {
   display: flex;
 }
-/* .item {
+.item {
   float: left;
   width: 25%;
 }
 
 .clear {
   clear: left;
-} */
+}
 
 .top-item {
   margin: 30px;
@@ -178,8 +384,11 @@ export default class Top extends Vue {}
 .top-item-icon p {
   position: absolute; /* item-iconからpを位置指定するための指定 */
   top: 35%;
-  width: 100%;
+  width: 250px;
   color: red;
+  font-size: 25px;
+
+  background-color: turquoise;
 }
 
 .top-item-icon img {
